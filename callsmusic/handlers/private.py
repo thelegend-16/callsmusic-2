259e9da5061efd63d61eb7d365226ff14648d62e
The Legend <75024331+thelegend-16@users.noreply.github.com>
2021-04-26 12:07:38 +0530

    Update private.py

--- callsmusic/handlers/private.py
+++ callsmusic/handlers/private.py
@@ -22,7 +22,7 @@ from ..helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""I am an open-source @CallsMusic bot, I let you play music in your group’s voice chat.
+        f"""I let you play music in your group’s voice chat.
 
 The commands I currently support are:
 
@@ -37,10 +37,10 @@ The commands I currently support are:
             [
                 [
                     InlineKeyboardButton(
-                        'Group', url='https://t.me/callsmusicchat',
+                        'Add me to your Group', url='https://t.me/Vchatprobot?startgroup=true',
                     ),
                     InlineKeyboardButton(
-                        'Channel', url='https://t.me/callsmusic',
+                        'Add my assistant', url='https://t.me/me_iz_zero',
                     ),
                 ],
             ],
